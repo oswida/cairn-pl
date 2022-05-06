c7f0c0a822e1ef7e037d665166dc53f8626b2e7a
Oskar Świda <oskar.swida@gmail.com>
2022-05-06 22:17:38 +0200

    relics cover

--- scripts/build-relics.sh
+++ scripts/build-relics.sh
@@ -11,14 +11,14 @@ cd $SCRIPT_DIR
 sed -i '/^###\s[^\n]*/d' tmp/*.md
 
 # PDF
-pandoc -V geometry:margin=2.5cm --variable papersize=A4 --variable title="Cairn: Artefakty" --variable subtitle="Przygotowany " --variable subtitle="$date" --variable subtitle="\linebreak przez Oskara Świda| Oryginał dostępny na: cairnrpg.com \linebreak CC-BY-SA 4.0" -V fontfamily:Alegreya  -f gfm --toc -s tmp/*.md -o "$SCRIPT_DIR/cairn-pl-relics.pdf"
+pandoc -V geometry:margin=2.5cm --variable papersize=A4 --variable title="Cairn: Artefakty" --variable subtitle="Przygotowany " --variable subtitle="$date" --variable subtitle="\linebreak przez Oskara Świda| Oryginał dostępny na: cairnrpg.com \linebreak CC-BY-SA 4.0" -V fontfamily:Alegreya -H relics-cover.tex -f gfm --toc -s tmp/*.md -o "$SCRIPT_DIR/cairn-pl-relics.pdf"
 
 # EPUB
 CSS_FILE=$SCRIPT_DIR/epub.css
 OUT_FILE=$SCRIPT_DIR/cairn-pl-relics.epub
 DOC_TITLE="Cairn PL: Artefakty"
 DOC_SUBTITLE="Przygotowany $date, Oskar Świda, Oryginał dostępny na: cairnrpg.com, CC-BY-SA 4.0"
-COVER_IMG=$SCRIPT_DIR/covers/cairn-relics.jpg
+COVER_IMG=$SCRIPT_DIR/covers/cover-relics-pf.jpg
 
 pandoc -c "$CSS_FILE" -M title="$DOC_TITLE" -M subtitle="$DOC_SUBTITLE" -f gfm --toc --toc-depth=4 --epub-embed-font Alegreya-Regular.ttf --epub-embed-font Alegreya-Italic.ttf --epub-embed-font Alegreya-Bold.ttf --epub-embed-font Alegreya-SemiBold.ttf --epub-cover-image "$COVER_IMG" -s tmp/*.md -o "$OUT_FILE"
 
